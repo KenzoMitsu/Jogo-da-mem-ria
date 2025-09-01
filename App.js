1f9898ea01416be791281243c980033226c70512
Julia de Almeida Tibério <juliatiberio94@gmail.com>
2025-09-01 17:27:43 -0300

    flcnsnvskifhsfwkjrf

--- App.js
+++ App.js
@@ -1,35 +1,26 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, TouchableOpacity, StyleSheet, FlatList } from "react-native";
-import Inicio from "./screens/Inicio"
-
 import { StyleSheet } from "react-native";
 import {NavigationContainer} from "@react-navigation/native";
 import {createStackNavigator} from "@react-navigation/stack";
-import Parabens from "./screens/Parabens";
-import Jogo from "./screens/Jogo"
+import Inicio from "./screens/Inicio";
+
 
 const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen options={{headerShown: false}} name={"Parabens"} component={Parabens} />
-        </Stack.Navigator>
-        <Stack.Navigator >
-          <Stack.Screen options={{ headerShown: false }}
-                        name={"Jogo"}
-                        component={Jogo} />
-        </Stack.Navigator>
-      </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen options={{headerShown: false}} name={"Inicio"} component={Inicio} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
