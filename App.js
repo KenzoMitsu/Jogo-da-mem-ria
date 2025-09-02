e4b5bd72fa5dab0c2df928c37a1dee6749e77037
davi <daviblasioli15@gmail.com>
2025-09-02 16:44:03 -0300

    a

--- App.js
+++ App.js
@@ -1,38 +1,24 @@
 import React from "react";
 import { StyleSheet } from "react-native";
-import {NavigationContainer} from "@react-navigation/native";
-import {createStackNavigator} from "@react-navigation/stack";
-import Inicio from "./screens/Inicio";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-// Importa todas as suas telas (screens)
+// Suas telas
+import Inicio from "./screens/Inicio";
 import Jogadores from "./screens/Jogadores.jsx";
 import Jogo from "./screens/Jogo.jsx";
 import Parabens from "./screens/Parabens.jsx";
 
-// Cria o navegador de pilha
 const Stack = createStackNavigator();
 
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Jogadores">
-                <Stack.Screen
-                    name="Jogadores"
-                    component={Jogadores}
-                    options={{ headerShown: false }}
-                />
-                <Stack.Screen
-                    name="Jogo"
-                    component={Jogo}
-                    options={{ headerShown: false }}
-                />
-                <Stack.Screen
-                    name="Parabens"
-                    component={Parabens}
-                    options={{ headerShown: false }}
-                />
-            <Stack.Navigator>
-                <Stack.Screen options={{headerShown: false}} name={"Inicio"} component={Inicio} />
+            <Stack.Navigator initialRouteName="Jogadores" screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Inicio" component={Inicio} />
+                <Stack.Screen name="Jogadores" component={Jogadores} />
+                <Stack.Screen name="Jogo" component={Jogo} />
+                <Stack.Screen name="Parabens" component={Parabens} />
             </Stack.Navigator>
         </NavigationContainer>
     );
@@ -41,8 +27,8 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
     },
 });
