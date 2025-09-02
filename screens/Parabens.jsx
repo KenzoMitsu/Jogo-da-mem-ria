7d3e566579d74236a0c6251e96d801cce771887d
Kauã <tefaamanda2020@gmail.com>
2025-09-02 17:18:42 -0300

    parabens

--- screens/Parabens.jsx
+++ screens/Parabens.jsx
@@ -8,13 +8,15 @@ export default function Inicio() {
 
     return (
         <ScrollView style={styles.container}>
-            <Image style={styles.img} source={require("../assets/logo.png")} />
-            <Text style={styles.textonorm}>PARABÉNS!</Text>
+            <View style={styles.imagemsec}>
+                <Image style={styles.img} source={require("../assets/logo.png")} />
+            </View>
+            <Text style={styles.parabens}>PARABÉNS!</Text>
             <Text style={styles.textonome}>JÚLIA{nome}</Text>
             <Text style={styles.textonorm}>Você foi o ganhador (a)!</Text>
             <View>
                <TouchableOpacity style={styles.btn}>
-                   <Text>
+                   <Text style={styles.textbtn}>
                        JOGAR NOVAMENTE
                    </Text>
                </TouchableOpacity>
@@ -27,33 +29,65 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: "#e5ead4",
     },
+    imagemsec: {
+        justifyContent: "center",
+        alignItems: "center",
+    },
     img: {
         marginTop: 50,
-        width: "100%",
-        height: "100%",
+        width: "70%",
+        height: "70%",
         alignItems: "center",
         justifyContent: "center",
         display: "flex",
         flexDirection: "row",
     },
+
+    parabens: {
+        fontFamily: "Arial",
+        fontSize: 30,
+        color: colors.verde_base,
+        fontWeight: "bold",
+        marginTop: 40,
+        textAlign: "center",
+    },
+
     textonorm: {
         fontFamily: "Arial",
         fontSize: 20,
         color: colors.verde_base,
+        marginTop: 10,
+        textAlign: "center",
+        fontWeight: "bold",
     },
     textonome: {
         fontFamily: "Arial",
-        fontSize: 20,
+        fontSize: 25,
         color: colors.verde_escuro,
+        fontWeight: "bold",
+        textAlign: "center",
+        marginTop: 10,
     },
     btn: {
         flexDirection: "row",
-        justifyContent: "space-between",
-        marginBottom: 20,
-        marginTop: 30,
-        borderBottomWidth: 1,
-        borderBottomColor: colors.verde_escuro,
-        paddingBottom: 20,
-        color: "black",
+        backgroundColor: colors.verde_base,
+        paddingBottom: 10,
+        paddingTop: 10,
+        paddingLeft: 20,
+        paddingRight: 20,
+        marginTop: 55,
+        marginLeft: 90,
+        marginRight: 90,
+        textAlign: "center",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 10,
+
+    },
+    textbtn: {
+        fontSize: 17,
+        fontFamily: "Arial",
+        fontWeight: "bold",
+        textAlign: "center",
     }
 })
